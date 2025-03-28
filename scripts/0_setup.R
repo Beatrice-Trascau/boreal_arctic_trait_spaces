e4fe756e1604a50c5ba5b8a2dcce16555366943c
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2025-03-28 15:28:08 +0100

    feat: added function to create file structure (if needed)

--- scripts/0_setup.R
+++ scripts/0_setup.R
@@ -28,4 +28,35 @@ package_vec <- c("here", "terra", "sf", "geodata", "mapview",
 # Execute the function
 sapply(package_vec, install_load_package)
 
+# 2. CREATE NECESSARY FILE STRUCTURE -------------------------------------------
+
+# Function to create the file structure needed to run the analysis smoothly
+create_project_structure <- function(base_path = "boreal_arctic_trait_space") {
+  # Define the directory structure
+  dirs <- c(
+    file.path(base_path),
+    file.path(base_path, "data"),
+    file.path(base_path, "scripts"),
+    file.path(base_path, "figure"),
+    file.path(base_path, "data", "raw_data"),
+    file.path(base_path, "data", "derived_data"),
+    file.path(base_path, "data", "raw_data", "biomes")
+  )
+  
+  # Create directories if they don't exist
+  for (dir in dirs) {
+    if (!dir.exists(dir)) {
+      dir.create(dir, recursive = TRUE)
+      cat("Created directory:", dir, "\n")
+    } else {
+      cat("Directory already exists:", dir, "\n")
+    }
+  }
+  
+  cat("\nProject structure setup complete!\n")
+}
+
+# Run function
+create_project_structure
+
 # END OF SCRIPT ----------------------------------------------------------------
