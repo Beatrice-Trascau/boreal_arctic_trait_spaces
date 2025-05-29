9e2c2c24baa9d423943cac91ef943e13645ff67e
beatrimt <beatrice.m.trascau@ntnu.no>
2025-05-29 14:11:53 +0200

    feat: changed to using a download request insted of direct download

--- scripts/2.1_GBIF_download.R
+++ scripts/2.1_GBIF_download.R
@@ -17,63 +17,69 @@ Sys.setenv(GBIF_USER = "my_username") # change with your details
 Sys.setenv(GBIF_PWD = "my_password")
 Sys.setenv(GBIF_EMAIL = "my_email")
 
-# 2. DOWNLOAD GBIF OCCURRENCES -------------------------------------------------
+# 2. PREPARE SPECIES LIST ------------.-----------------------------------------
 
-# Define polar projection
-polarproj <- '+proj=laea +lat_0=90 +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0'
-
-# List of northern countries
-northern_countries <- c('NO', 'SE', 'FI','RU','CA','IS','GL','SJ','MN','JP')
-
-# Extract list of species from df
+# Get species list
 species_list <- unique(corrected_species_list$CheckedSpeciesName)
 
-# Function to download GBIF occurrences for each species
-download_species_occs <- function(species_name, verbose = TRUE){
-  if(verbose) cat("Downloading:", species_name, "\n")
-  # search for occurrences
-  gbif_result <- occ_search(scientificName = species_name,
-                            hasCoordinate = TRUE,
-                            hasGeospatialIssue = FALSE,
-                            decimalLatitude = "40,90",
-                            decimalLongitude = "-180,180",
-                            basisOfRecord = c("HUMAN_OBSERVATION", "OBSERVATION", 
-                                              "MACHINE_OBSERVATION", "PRESERVED_SPECIMEN", 
-                                              "LIVING_SPECIMEN"),
-                            fields = c("key", "scientificName", "decimalLatitude",
-                                       "decimalLongitude", "year", "month", "country", 
-                                       "basisOfRecord", "coordinateUncertaintyInMeters"))
+# Convert species names to GBIF taxon keys
+taxon_keys <- c()
+failed_species <- c()
+
+for(i in 1:length(species_list)){
+  # get taxon keys for each species
+  key_lookup <- name_backbone(species_list[i])
   
-  # check if data was returned
-  if(gbif_result$meta$count == 0) {
-    if(verbose) cat("  No occurrences found for:", species_name, "\n")
-    return(NULL)
+  # store in either taxon_keys or failed_species based on success
+  if(!is.null(key_lookup$usageKey) && key_lookup$matchType != "NONE"){
+    taxon_keys <- c(taxon_keys, key_lookup$usageKey)
+  } else {
+    failed_species <- c(failed_species, species_list[i])
   }
-  
-  # extract data
-  occurrences <- gbif_result$data
-  
-  # quick data clean
-  occurrences_clean <- occurrences |>
-    # remove any records without coordinates
-    filer(!is.na(decimalLatitude), !is.na(decimalLongitude)) |>
-    # remove records with high coordinate uncertainty 
-    filter(is.na(coordinateUncertaintyInMeters) | coordinateUncertaintyInMeters <= 10000) |>
-    # add clean species name
-    mutate(species_clean = gsub(" ", ".", species_name)) |>
-    # select relevant columns
-    select(species = scientificName, species_clean, decimalLongitude, decimalLatitude, 
-           year, month, country, basisOfRecord, coordinateUncertaintyInMeters)
-  
-  # get information about how many records were cleaned out
-  if(verbose) {
-    cat("  Found:", nrow(gbif_result$data), "total records\n")
-    cat("  After cleaning:", nrow(occurrences_clean), "records\n")
-  }
-  
-  return(occurrences_clean)
 }
 
-# Download GBIF data
-gbif_results <- download_species_occs(species_list)
+# Print failed species
+if(length(failed_species) > 0){
+  print(failed_species)
+}
 
+# 3. CREATE DOWNLOAD REUQEST ---------------------------------------------------
+
+# Create download request
+download_key <- occ_download(pred_in("taxonKey", taxon_keys),
+                             pred_gte("decimalLatitude", 40),
+                             pred_lte("decimalLatitude", 90),
+                             pred("hasCoordinate", TRUE),
+                             format = "DWCA")
+
+# Save download key
+cat("Download key:", download_key, "\n") #0006611-250525065834625
+
+# Check download status
+occ_download_wait(download_key)
+
+# Get download metadata
+download_meta <- occ_download_meta(download_key)
+
+# 4. IMPORT AND SAVE DATA ------------------------------------------------------
+
+# Import the download data
+occurrence_species <- occ_download_get(download_key) |>
+  occ_download_import()
+
+# Save the data
+save(occurrence_species, here::here("data", "raw_data", 
+                                    "occurrence_species_northern_hemisphere.rda"))
+
+# Save download key metadata
+download_info <- list(download_key = download_key,
+                      species_list = species_list,
+                      taxon_keys = taxon_keys,
+                      failed_species = failed_species,
+                      download_date = Sys.Date(),
+                      total_records = nrow(occurrence_species))
+
+# Save download info
+save(download_info, file = here::here("data", "raw_data", "download_info_species.rda"))
+
+# END OF SCRIPT ----------------------------------------------------------------
