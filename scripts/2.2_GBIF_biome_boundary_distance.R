03922ac928e93a40a48e4aef5dae94c933b9b081
beatrimt <beatrice.m.trascau@ntnu.no>
2025-06-04 16:06:43 +0200

    faeat: new script to calculate distance to biome boundary

---
+++ scripts/2.2_GBIF_biome_boundary_distance.R
@@ -0,0 +1,25 @@
+##----------------------------------------------------------------------------##
+# PAPER 3: BOREAL AND ARCTIC PLANT SPECIES TRAIT SPACES 
+# 2.2_GBIF_biome_boundary_distance
+# This script contains code which calcualtes the distance to biome boundary for
+# each species in our list
+##----------------------------------------------------------------------------##
+
+# 1. SETUP ---------------------------------------------------------------------
+
+# Load packages and functions
+library(here)
+source(here("scripts", "0_setup.R"))
+
+# Load species list 
+load(here("data", "derived_data", "all_filtered_standardised_species.RData"))
+
+# Load WWF biomes
+# Citation: Olson, D. M., Dinerstein, E., Wikramanayake, E. D., Burgess, N. D., Powell, G. V. N., Underwood, E. C., D'Amico, J. A., Itoua, I., Strand, H. E., Morrison, J. C., Loucks, C. J., Allnutt, T. F., Ricketts, T. H., Kura, Y., Lamoreux, J. F., Wettengel, W. W., Hedao, P., Kassem, K. R. 2001. Terrestrial ecoregions of the world: a new map of life on Earth. Bioscience 51(11):933-938.
+global_biomes <- st_read (here("data", "raw_data", "biomes", "wwf_terr_ecos.shp"))
+
+#Load Boreal Forest (BIOME = 6)
+boreal_forest <- st_union(global_biomes[global_biomes$BIOME == 6,])
+
+# Load Tundra (BIOME = 11)
+tundra <- st_union(global_biomes[global_biomes$BIOME == 11 &(global_biomes$REALM == "PA"|global_biomes$REALM == "NA"), ])
