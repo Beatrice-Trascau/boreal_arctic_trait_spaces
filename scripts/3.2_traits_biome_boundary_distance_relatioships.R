278b74d46af1ecb306cb4afb89b8ce3277a76495
Beatrice-Trascau <beatrice.m.trascau@ntnu.no>
2025-07-30 11:01:05 +0200

    feat: new script to explore relationships between trait values and distances to biome boundaries

---
+++ scripts/3.2_traits_biome_boundary_distance_relatioships.R
@@ -0,0 +1,6 @@
+##----------------------------------------------------------------------------##
+# PAPER 3: BOREAL AND ARCTIC PLANT SPECIES TRAIT SPACES 
+# 3.2_traits_biome_boundary_distance_relationships
+# This script contains code which explores ther relationships between species
+# trait values and their mean distance to biome boundaries
+##----------------------------------------------------------------------------##
